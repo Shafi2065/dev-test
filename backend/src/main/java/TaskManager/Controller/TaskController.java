7f3f8e8fe6014d69692bf7d43d8d37f7fa5fcef9
Shafi_2065 <shafimiah234@gmail.com>
2025-06-17 06:26:09 +0100

    fixed the backend directories/sub directories
    Made a successful API call to the backend from the front end dashboard

--- backend/src/main/java/TaskManager/Controller/TaskController.java
+++ backend/src/main/java/TaskManager/Controller/TaskController.java
@@ -1,7 +1,7 @@
-package Controller;
+package TaskManager.Controller;
 
-import Model.Task;
-import Repository.TaskRepository;
+import TaskManager.Model.Task;
+import TaskManager.Repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
